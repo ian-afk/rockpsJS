083311f711758a8298f52677d3b70b6176a710f4
ian-afk <crkdosal@gmail.com>
2024-04-14 22:30:00 +0800

    add playRound function to determine whos the round winner

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const getComputerChoice = () => {
   console.log(random);
   return choices[random];
 };
+
+const playRound = (playerSelection, computerSelection) => {
+  const player = playerSelection.toLowerCase();
+  const computer = computerSelection.toLowerCase();
+  if (player === "rock" && computer === "paper")
+    return ["lose", `You Lose! Paper beats Rock`];
+  else if (player === "paper" && computer === "scissors") {
+    return ["lose", `You Lose! Scissors beats Paper`];
+  } else if (player === "scissors" && computer === "rock") {
+    return ["lose", `You Lose! Rock beats scissors`];
+  } else if (player === computer) {
+    return ["draw", `Draw! ${player} ${computer}`];
+  } else {
+    return ["win", `You won! ${player} beats ${computer}`];
+  }
+};
